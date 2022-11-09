14968a465b333e4ab96388299c1696ef055b5d94
nandobfer <nandobfer@yahoo.com.br>
2022-11-09 12:01:02 -0300

    feat: added form and input

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,40 @@
+import { Form, Input } from 'react-burgos';
 import { useNavigate } from 'react-router-dom';
 import './style.scss';
 
 export const Home = () => {
 
     const navigate = useNavigate();
+
+    const onFormSubmit = (values) => {
+        alert(JSON.stringify(values, null, 2))
+    }
+
+    const inputs = {
+        input_login: '',
+        input_senha: '',
+    }
     
     return (
         <div className='body'>
             <h1>Home</h1>
-            <form>
-                <div>
-                    <label htmlFor="user-input">Usuário</label>
-                    <input id='user-input' type="text" />
-                </div>
-                <div>
-                    <label htmlFor="password-input">Senha</label>
-                    <input id='password-input' type="text" />
-                </div>
-                <button type="submit">Entrar</button>
-            </form>
+            <Form initialValues={inputs} onSubmit={values => onFormSubmit(values)}>
+                        <label htmlFor="input_login">Usuário</label>
+                        <Input 
+                            mask={() => false}
+                            id='input_login'
+                            placeholder='Usuário, e-mail ou CPF'
+                            className='default-input'
+                            />
+                        <label htmlFor="input_senha">Senha</label>
+                        <Input 
+                            mask={() => false}
+                            id='input_senha'
+                            placeholder='Senha'
+                            className='default-input'
+                        />
+                        <button className='default-button' type="submit">Entrar</button>
+                    </Form>
         </div>
     )
 }
