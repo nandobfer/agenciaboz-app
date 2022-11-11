31401ae85e17b2c9f8e7ae2ce1b66e0d663bdc84
nandobfer <nandobfer@yahoo.com.br>
2022-11-11 16:55:59 -0300

    feat: added user homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
 import { Users } from "./pages/Users";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
+        <Route index element={<Login />} />
         <Route path='/home' element={<Home />} />
         <Route path='/usuarios' element={<Users />} />
       </Routes>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,54 +1,29 @@
-import { useState } from 'react';
-import { Form, Input } from 'react-burgos';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { api } from '../../api';
 import './style.scss';
 
 export const Home = () => {
 
-    const navigate = useNavigate();
-    const [feedback, setFeedback] = useState('')
+    const navigate = useNavigate()
+    const user = useLocation().state.user
 
-    const onFormSubmit = (values) => {
-        api.post('/login', { user: values.input_login, password: values.input_senha })
-            .then((response) => {
-                if (response.data.error) {
-                    setFeedback(response.data.error)
-                }
-            })
-            .catch((error) => {
-                alert(error)
-            })
+    const toUsers = () => {
+        api.post('/usuarios', {})
+        .then((response) => {
+            if (response.data.error) alert(JSON.stringify(response.data.error, null, 2))
+
+            navigate('/usuarios', {state: {users: response.data}})
+        })
+        .catch((error) => {
+            alert(JSON.stringify(error, null, 2))
+        })
     }
-
-    const inputs = {
-        input_login: '',
-        input_senha: '',
-    }
-
+    
     return (
-        <div className='main-container'>
-            <img src="boz-LogoBranco.png" alt="BOZ" />
-            <Form initialValues={inputs} onSubmit={values => onFormSubmit(values)}>
-                <label htmlFor="input_login">Usuário</label>
-                <Input
-                    mask={() => false}
-                    id='input_login'
-                    placeholder='Usuário, e-mail ou CPF'
-                    className='default-input'
-                />
-                <label htmlFor="input_senha">Senha</label>
-                <Input
-                    mask={() => false}
-                    id='input_senha'
-                    placeholder='Senha'
-                    className='default-input'
-                />
-                <button className='default-button' type="submit">Entrar</button>
-                <p>{feedback}</p>
-
-            </Form>
-
-        </div>
+        <section className="home-page">
+            <h1>{user.name}</h1>
+            <button onClick={() => navigate('/')}>Sair</button>
+            <button onClick={toUsers}>Usuários</button>
+        </section>
     )
 }
