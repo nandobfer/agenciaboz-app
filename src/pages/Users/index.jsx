8f49cf7964bcfea38b7f5924305d7b74bf4275aa
nandobfer <nandobfer@yahoo.com.br>
2022-11-11 16:56:58 -0300

    feat: added list from api

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,29 +1,55 @@
+import { useLocation } from "react-router-dom"
 import { Table } from "../../components/Table"
 
 export const Users = () => {
 
-    const data_test = [
-        { nome: 'Fernando', idade: 24 }
-    ]
+    const users = useLocation().state.users
+    console.log(users)
+
+    const formatCPF = (cpf) => {
+        if (cpf == 'None') {
+            return null
+        }
+        return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
+    }
+
+    const formatZero = (number) => {
+        return Math.floor(number/10) ? number : `0${number}`
+    }
+
+    const formatBirthday = (datetime) => {
+        const date = new Date(datetime)
+        const day = formatZero(date.getDate())
+        const month = formatZero(date.getMonth()+1)
+        const year = date.getFullYear()
+        return `${day}/${month}/${year}`
+    }
 
     const columns = [
         {
             name: 'Nome',
-            selector: row => row.nome,
+            selector: row => row.name,
             sortable: true,
         },
         {
-            name: 'Idade',
-            selector: row => row.idade,
+            name: 'CPF',
+            selector: row => row.cpf,
             sortable: true,
+            format: row => formatCPF(row.cpf)
+        },
+        {
+            name: 'Data de nascimento',
+            selector: row => row.birthday,
+            sortable: true,
+            format: row => formatBirthday(row.birthday)
         },
     ]
 
     return (
         <section>
-            <h1>teste</h1>
+            <h1>Painel de Usuários</h1>
             <Table
-                data={data_test}
+                data={users}
                 columns={columns}
             />
         </section>
