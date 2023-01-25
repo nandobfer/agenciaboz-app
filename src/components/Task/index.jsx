127f9935d7d4e5cbc2427fb1ff3688c848120a33
nandobfer <nandobfer@yahoo.com.br>
2023-01-25 11:05:28 -0300

    feat: added date to tasks

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -4,6 +4,7 @@ import './style.scss';
 
 export const Task = ({ task }) => {
     const team = useTeam().value
+    const date = new Date(task.date)
     
     return (
         <div className='Task-Component' >
@@ -11,7 +12,7 @@ export const Task = ({ task }) => {
                 <div className="task-data">
                     <input type="radio" name="" id="teste" />
                     <p>{task.title}</p>
-                    <p className="task-date">Qua, 25/01/2023</p>
+                    <p className="task-date">{date.toLocaleDateString('pt-BR', { weekday: 'long' }).split('-')[0]}, {date.toLocaleDateString()}</p>
                 </div>
                 <hr />
                 <div className="tasked-person">
